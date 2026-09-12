079fe19523be5624ee7b67748548795183bce8e9
Mohd Kaif <mohdkaifmohdkaif22@gmail.com>
2026-09-12 01:56:01 -0700

    Find a specific element

---
+++ array4.py
@@ -0,0 +1,12 @@
+arr=[22,99,23,67]
+element=int(input("Enter Element:"))
+for i in arr:
+        if i==element:
+                print("Found:",i)
+                break
+        else:
+                print("Not found")
+                break
+        
+
+        
