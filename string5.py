72245d33a3a75a4bd18334b466fe06c2eba9e47d
Mohd Kaif <mohdkaifmohdkaif22@gmail.com>
2026-09-17 00:36:57 -0700

    find duplicate string

---
+++ string5.py
@@ -0,0 +1,8 @@
+st=input("enter string: ")
+for i in range(len(st)):
+        count=0
+        for j in range(len(st)):
+                if st[i]==st[j]:
+                        count+=1
+        if count>1 and st[i] not in st[:i]:
+                 print(st[i],":",count)
